224a073a97d4bb45cb0059cb299fe1a62ea4f56e
Rahul Baphana <rahul.kbaphana@yahoo.com>
2017-10-08 19:48:52 +0530

    Varargs not supported for lambda

--- app/com/expense/tracker/ExpenseTracker.scala
+++ app/com/expense/tracker/ExpenseTracker.scala
@@ -2,5 +2,5 @@ package com.expense.tracker
 
 case class ExpenseTracker(totalAmount: Int = 0) {
 
-  def add = (expenses: Expense*) => ExpenseTracker(expenses.toList.reduce((a,b) => Expense(a.price + b.price)).price)
+  def add(expenses: Expense*) = ExpenseTracker(expenses.toList.reduce((a,b) => Expense(a.price + b.price)).price)
 }
